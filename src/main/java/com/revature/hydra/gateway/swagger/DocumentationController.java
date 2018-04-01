97ac39bbb17ab66ac553b432666329041980cea9
Bryton Hall <email@bryton.io>
2018-04-01 14:55:04 -0400

    add routing for trainer and unavailable services

--- src/main/java/com/revature/hydra/gateway/swagger/DocumentationController.java
+++ src/main/java/com/revature/hydra/gateway/swagger/DocumentationController.java
@@ -18,20 +18,25 @@ public class DocumentationController implements SwaggerResourcesProvider{
 	@Override
     public List<SwaggerResource> get() {
 		List<SwaggerResource> resources = new ArrayList<>();
-		// resources.add(swaggerResource("trainer", "/api/trainer/v2/api-docs", "2.0"));
+
+		// A bit confusing here: the 'v2' below refers to Swagger version 2!
+
+		resources.add(swaggerResource("trainer", "/api/trainer/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("trainee", "/api/trainee/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("types", "/api/types/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("assessment", "/api/assessment/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("grade", "/api/grade/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("batch", "/api/batch/v0/api-docs", "2.0"));
-		resources.add(swaggerResource("address", "/api/address/v0/api-docs", "2.0"));
+		resources.add(swaggerResource("batch", "/api/batch/v2/api-docs", "2.0"));
+		resources.add(swaggerResource("address", "/api/address/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("pdf", "/api/pdf/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("salesforce", "/api/salesforce/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("note", "/api/note/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("category", "/api/category/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("panel", "/api/panel/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("reporting", "/api/reporting/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("curriculum", "/api/curriculum/v0/api-docs", "2.0"));
+		resources.add(swaggerResource("curriculum", "/api/curriculum/v2/api-docs", "2.0"));
+		resources.add(swaggerResource("skills", "/api/skills/v2/api-docs", "2.0"));
+		resources.add(swaggerResource("unavailable", "/api/unavailable/v2/api-docs", "2.0"));
 		
 
 		return resources;
