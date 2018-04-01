2f8862c28c21e120c1ba23a6a5ae9216a5fef5df
Bryton Hall <email@bryton.io>
2018-04-01 13:28:30 -0400

    add address and curriculum service routes

--- src/main/java/com/revature/hydra/gateway/swagger/DocumentationController.java
+++ src/main/java/com/revature/hydra/gateway/swagger/DocumentationController.java
@@ -18,19 +18,20 @@ public class DocumentationController implements SwaggerResourcesProvider{
 	@Override
     public List<SwaggerResource> get() {
 		List<SwaggerResource> resources = new ArrayList<>();
-		resources.add(swaggerResource("trainer", "/trainer/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("trainee", "/trainee/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("types", "/types/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("assessment", "/assessment/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("grade", "/grade/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("batch", "/batch/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("address", "/address/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("pdf", "/pdf/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("salesforce", "/salesforce/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("note", "/note/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("category", "/category/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("panel", "/panel/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("reporting", "/reporting/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("trainer", "/api/trainer/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("trainee", "/api/trainee/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("types", "/api/types/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("assessment", "/api/assessment/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("grade", "/api/grade/v2/api-docs", "2.0"));
+		resources.add(swaggerResource("batch", "/api/batch/v0/api-docs", "2.0"));
+		resources.add(swaggerResource("address", "/api/address/v0/api-docs", "2.0"));
+		// resources.add(swaggerResource("pdf", "/api/pdf/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("salesforce", "/api/salesforce/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("note", "/api/note/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("category", "/api/category/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("panel", "/api/panel/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("reporting", "/api/reporting/v2/api-docs", "2.0"));
+		resources.add(swaggerResource("curriculum", "/api/curriculum/v0/api-docs", "2.0"));
 		
 
 		return resources;
