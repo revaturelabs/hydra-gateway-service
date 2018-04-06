a326b40cc3deaf7409effab73e2884c251198d50
Kevin Hsieh <kevin.b.hsieh@gmail.com>
2018-04-06 11:52:51 -0400

    Dev to Master (#5)
    * Removing zuul filters
    
    * forcing build
    
    * yml fix
    
    * ip fix
    
    * uri fix
    
    * remove test
    
    * removed one test
    
    * Boot cleanup (#3)
    
    * Microservice configurations merge. (#4)
    
    * Fix docker connections.
    
    * Add custom configs.
    
    * Fix default zone error.
    
    * Update routes.
    
    * Change topic url to subtopic.
    
    * Add calendar routes.
    
    * Change Eureka routes.
    
    * Update properties.
    
    * Change Eureka defualt zone.
    
    * changed port to localhost
    
    * Update to standard .gitignore v1
    
    * Updating .gitignore (#2)
    
    removing *'s
    
    * Updated ReadMe.md, added JavaDocs and code cleanup

--- src/main/java/com/revature/hydra/gateway/swagger/DocumentationController.java
+++ src/main/java/com/revature/hydra/gateway/swagger/DocumentationController.java
@@ -10,13 +10,21 @@ import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
+/***
+ * DocumentationController is responsible for establishing Swagger
+ * documentation. Swagger ui can be accessed at:
+ * 		http://localhost:8800/swagger-ui.html 
+ *  
+ * @author Unknown
+ */
+
 @Component
 @Primary
 @EnableAutoConfiguration
-public class DocumentationController implements SwaggerResourcesProvider{
-	
+public class DocumentationController implements SwaggerResourcesProvider {
+
 	@Override
-    public List<SwaggerResource> get() {
+	public List<SwaggerResource> get() {
 		List<SwaggerResource> resources = new ArrayList<>();
 		resources.add(swaggerResource("trainer", "/trainer/v2/api-docs", "2.0"));
 		resources.add(swaggerResource("trainee", "/trainee/v2/api-docs", "2.0"));
@@ -31,16 +39,16 @@ public class DocumentationController implements SwaggerResourcesProvider{
 		resources.add(swaggerResource("category", "/category/v2/api-docs", "2.0"));
 		resources.add(swaggerResource("panel", "/panel/v2/api-docs", "2.0"));
 		resources.add(swaggerResource("reporting", "/reporting/v2/api-docs", "2.0"));
-		
 
 		return resources;
 	}
-	
+
 	private SwaggerResource swaggerResource(String name, String location, String version) {
-        SwaggerResource swaggerResource = new SwaggerResource();
-        swaggerResource.setName(name);
-        swaggerResource.setLocation(location);
-        swaggerResource.setSwaggerVersion(version);
-        return swaggerResource;
-    }
+		SwaggerResource swaggerResource = new SwaggerResource();
+		swaggerResource.setName(name);
+		swaggerResource.setLocation(location);
+		swaggerResource.setSwaggerVersion(version);
+
+		return swaggerResource;
+	}
 }
