e6bec06036f4daa24295a0655b7812f2e4b7e703
Bryton Hall <email@bryton.io>
2018-04-08 19:57:16 -0400

    load the common swagger documentation

--- src/main/java/com/revature/hydra/gateway/config/DocumentationController.java
+++ src/main/java/com/revature/hydra/gateway/config/DocumentationController.java
@@ -37,12 +37,14 @@ public class DocumentationController implements SwaggerResourcesProvider{
 
 		// A bit confusing here: the 'v2' below refers to Swagger version 2!
 
-		resources.add(swaggerResource("address", "/api/address/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("batch", "/api/batch/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("curriculum", "/api/curriculum/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("skills", "/api/skills/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("trainer", "/api/trainer/v2/api-docs", "2.0"));
-		resources.add(swaggerResource("unavailable", "/api/unavailable/v2/api-docs", "2.0"));
+		resources.add(swaggerResource("", "/api/v2/api-docs", "2.0"));
+
+		// resources.add(swaggerResource("address", "/api/address/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("batch", "/api/batch/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("curriculum", "/api/curriculum/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("skills", "/api/skills/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("trainer", "/api/trainer/v2/api-docs", "2.0"));
+		// resources.add(swaggerResource("unavailable", "/api/unavailable/v2/api-docs", "2.0"));
 
 		// resources.add(swaggerResource("trainee", "/api/trainee/v2/api-docs", "2.0"));
 		// resources.add(swaggerResource("types", "/api/types/v2/api-docs", "2.0"));
