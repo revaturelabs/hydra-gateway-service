8de61c9d04987a25263220504e0c08853835b931
Robert Hamel <roberthamel@gmail.com>
2018-04-09 14:56:33 -0400

    disable headers

--- src/main/java/com/revature/hydra/gateway/config/SecurityConfig.java
+++ src/main/java/com/revature/hydra/gateway/config/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.revature.hydra.gateway.config;
 
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -22,18 +20,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         JwtWebSecurityConfigurer
-                .forRS256(apiAudience, issuer)
-                .configure(http)
-                .authorizeRequests()
-                .antMatchers(
-                    HttpMethod.GET,
-                    "/swagger-ui.*",
-                    "/webjars/springfox-swagger-ui/**",
-                    "/swagger-resources/**",
-                    "/api/v2/api-docs",
-                    "/v2/api-docs"
-                ).permitAll()
-                .anyRequest().authenticated();
+            .forRS256(apiAudience, issuer)
+            .configure(http)
+            .headers().disable()
+            .authorizeRequests()
+            .antMatchers(
+                HttpMethod.GET,
+                "/swagger-ui.*",
+                "/webjars/springfox-swagger-ui/**",
+                "/swagger-resources/**",
+                "/api/v2/api-docs",
+                "/v2/api-docs"
+            ).permitAll()
+            .antMatchers(HttpMethod.OPTIONS).permitAll()
+            .anyRequest().authenticated();
     }
 
 }
